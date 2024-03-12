d80e6a0a92be415e263b6a6e999e9ffb7c22953f
schoenfelddev <schnfeldanton@gmail.com>
2024-03-12 19:14:55 +0100

    Redocumented Texture and made it use the newly created Texture parameter classes

--- src/main/java/org/nebula/graphics/globjects/FrameBuffer.java
+++ src/main/java/org/nebula/graphics/globjects/FrameBuffer.java
@@ -14,7 +14,7 @@ public class FrameBuffer extends OpenGLObject {
 
         glBindFramebuffer(GL_FRAMEBUFFER, id);
 
-        texture = new Texture(width, height);
+        texture = new Texture(new TextureDimensions(width, height));
 
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture.id, 0);
         texture.unbind();
