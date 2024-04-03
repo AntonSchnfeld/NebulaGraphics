d7ade30289c0c11f7cf3b1afd1cd253bd5fe002d
schoenfelddev <schnfeldanton@gmail.com>
2024-04-03 02:23:21 +0200

    Removed empty constructor and annotated attributes with @NonNull

--- src/main/java/com/github/nebula/graphics/data/UniformAttribute.java
+++ src/main/java/com/github/nebula/graphics/data/UniformAttribute.java
@@ -1,12 +1,11 @@
 package com.github.nebula.graphics.data;
 
+import io.reactivex.rxjava3.annotations.NonNull;
+
 /**
  * @param dataType
  * @param name
  * @author Anton Schoenfeld
  * @since 22.03.2024
  */
-public record UniformAttribute(GLDataType dataType, String name) {
-    public UniformAttribute {
-    }
-}
+public record UniformAttribute(@NonNull GLDataType dataType, @NonNull String name) {}
