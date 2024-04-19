97b2318e317d55156a1a4a9e149ca92e79f51870
SchnfeldDev <schnfeldanton@gmail.com>
2024-04-19 23:20:40 +0200

    Started reimplementing Model.renderInstances and defined the next steps in TODOs

--- src/main/java/com/github/nebula/graphics/Model.java
+++ src/main/java/com/github/nebula/graphics/Model.java
@@ -2,19 +2,15 @@ package com.github.nebula.graphics;
 
 import com.github.nebula.graphics.data.GLDataType;
 import com.github.nebula.graphics.globjects.Buffer;
-import com.github.nebula.graphics.globjects.VertexArray;
-import com.github.nebula.graphics.util.BufferUtil;
 import io.reactivex.rxjava3.annotations.NonNull;
 import lombok.Getter;
 import lombok.val;
-import org.lwjgl.opengl.GL42C;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.nio.FloatBuffer;
+import java.util.*;
 
 import static org.lwjgl.opengl.GL15C.*;
+import static org.lwjgl.opengl.GL30C.GL_MAP_WRITE_BIT;
 import static org.lwjgl.opengl.GL43C.GL_SHADER_STORAGE_BUFFER;
 
 /**
@@ -22,9 +18,11 @@ import static org.lwjgl.opengl.GL43C.GL_SHADER_STORAGE_BUFFER;
  * @since 26.03.2024
  */
 public class Model implements AutoCloseable {
+    private static final int MAT4F_SIZE = 16;
     private final @Getter Map<Material, List<Mesh>> meshMaterialMap;
     private final List<ModelInstance> instances;
     private final Buffer mat4Ssbo;
+    private GPUMesh concatMesh;
     private int oldNumInstances;
 
     public Model(@NonNull Map<Material, List<Mesh>> modelMaterialMap) {
@@ -46,36 +44,29 @@ public class Model implements AutoCloseable {
     }
 
     public void renderInstances() {
+        // Reupload transformation matrices if new instances were added
+        // TODO: Handle dynamically changing transformation matrices in ModelInstances
+        // TODO: e.g. when a meteroid is moving continously, the transformation matrix
+        // TODO: has to be reuploaded
         if (oldNumInstances != instances.size()) {
             oldNumInstances = instances.size();
-            mat4Ssbo.data(oldNumInstances, GL_DYNAMIC_DRAW, GLDataType.MAT4);
-        }
+            long size = (long) oldNumInstances * MAT4F_SIZE;
+            mat4Ssbo.data(size, GL_DYNAMIC_DRAW, GLDataType.MAT4);
 
-        val mappedSsbo = mat4Ssbo.map(GL_WRITE_ONLY).asFloatBuffer();
-        // Populate Ssbo with transformation matrices
-        for (int i = 0; i < instances.size(); i++) {
-            val transformMatrix = instances.get(i).getTransformationMatrix();
-            mappedSsbo.put(i * 16, transformMatrix.get(new float[16]));
+            FloatBuffer mappedMat4Ssbo = mat4Ssbo.mapRange(GL_MAP_WRITE_BIT, 0, (int) size).asFloatBuffer();
+            for (int i = 0; i < oldNumInstances; i++) {
+                ModelInstance instance = instances.get(i);
+                mappedMat4Ssbo.put(0, instance.getTransformationMatrix().get(new float[MAT4F_SIZE]));
+            }
         }
-        mat4Ssbo.unmap();
-
-        for (val material : meshMaterialMap.keySet()) {
-            val meshes = meshMaterialMap.get(material);
-            val resultMesh = BufferUtil.concatMeshesIntoMesh(new GPUMesh(), meshes.toArray(new Mesh[meshes.size() - 1]));
-            resultMesh.setIndices(BufferUtil.getDefaultIndexBuffer((int) resultMesh.getVerticesSize()));
-            val vao = new VertexArray();
-            material.getShader().getVertexAttributes().format(vao, resultMesh.getVbo());
-            // TODO: Upload mat4Ssbo into the shader
-            vao.bind();
-            resultMesh.getEbo().bind();
-            material.bind();
-            GL42C.glDrawElementsInstanced(GL_TRIANGLES, (int) resultMesh.getIndicesSize(), GL_UNSIGNED_INT, 0L, oldNumInstances);
-            vao.close();
-        }
+        // TODO: Upload vertices and indices of meshMaterialMap into concatMesh
     }
 
-    public void render() {
-
+    private boolean isDirty() {
+        for (List<Mesh> meshes : meshMaterialMap.values())
+            for (Mesh mesh : meshes)
+                if (mesh.dirty) return true;
+        return false;
     }
 
     @Override
